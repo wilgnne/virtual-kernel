926f0c9b5df2ad62d8077688dac7fc816d93807d
Wilgnne Khawan <wilgnne.kba@gmail.com>
2021-05-07 20:01:43 +0000

    feat: Use Next link end Chakra link

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-const BACKEND_URL = isProd ? 'virtual-kernel/' : ''
+const BACKEND_URL = isProd ? '/virtual-kernel' : ''
 
 module.exports = {
   assetPrefix: BACKEND_URL,
--- src/components/Navbar/MenuItem.tsx
+++ src/components/Navbar/MenuItem.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import Link from 'next/link'
-import { Text } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { Link, Text } from '@chakra-ui/react'
 
 const MenuItem = ({ children, to = '/', ...rest }) => {
   return (
-    <Link href={to}>
-      <Text display="block" {...rest}>
-        {children}
-      </Text>
-    </Link>
+    <NextLink href={to}>
+      <Link>
+        <Text display="block" {...rest}>
+          {children}
+        </Text>
+      </Link>
+    </NextLink>
   )
 }
 
