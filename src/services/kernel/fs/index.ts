7c70b498f50c61d421aedd2a31576362e790c703
Wilgnne Khawan <wilgnne.kba@gmail.com>
2021-05-07 15:46:57 +0000

    feat: FileSystem

--- src/services/kernel/fs/index.ts
+++ src/services/kernel/fs/index.ts
@@ -1,14 +1,10 @@
 import { dirname, basename, resolve } from 'path'
-//import { writeFileSync } from 'fs'
 
-import create_node, { nodeType } from "./iNode";
-import { read_file, write_file } from "./file";
-import { findFreeBlock, read_iNode, write_iNode } from "./disk";
-import { copy_buffer, printArrayBuffer } from "./utils";
-import { fsNode, get_node, iNodeSizeFromFsNode } from "./fsTree";
-
-const block_size = 400
-const blocks = 32 * 128
+import create_node, { get_inode_size, nodeType } from './iNode'
+import { read_file, write_file } from './file'
+import { findFreeBlock, freeBlock, read_iNode, write_iNode } from './disk'
+import { copy_buffer, printArrayBuffer } from './utils'
+import { fsNode, get_node, iNodeSizeFromFsNode } from './fsTree'
 
 export type FSCallback = (fileSystemkernel: FileSystem) => void
 
@@ -31,7 +27,7 @@ class FileSystem {
 
     this.fileTree = {
       iNode: create_node('/', nodeType.folder),
-      childrens: []
+      childrens: [],
     }
 
     this.writeFS()
@@ -66,10 +62,10 @@ class FileSystem {
     return offset
   }
 
-  private writeFS() {
-    this.writeFsNode(this.fileTree);
-    this.readFS();
-    this.callbaks.forEach(callbak => callbak(this));
+  writeFS() {
+    this.writeFsNode(this.fileTree)
+    this.readFS()
+    this.callbaks.forEach((callbak) => callbak(this))
   }
 
   private readFsNode(offset = 0): fsNode {
@@ -77,7 +73,7 @@ class FileSystem {
 
     const rootFS: fsNode = {
       iNode: root,
-      childrens: []
+      childrens: [],
     }
 
     if (root.nodeType === nodeType.folder) {
@@ -98,12 +94,13 @@ class FileSystem {
     const wd_node = get_node(wd, this.fileTree)
     const parentNode = get_node(dirname(path), wd_node)
 
-    if (parentNode === undefined) return [false, `mkdir: ${dirname(path)}: No such file or directory`]
+    if (parentNode === undefined)
+      return [false, `mkdir: ${dirname(path)}: No such file or directory`]
 
     const name = basename(path)
     const new_dir: fsNode = {
       iNode: create_node(name, nodeType.folder),
-      childrens: []
+      childrens: [],
     }
 
     parentNode.childrens.push(new_dir)
@@ -113,14 +110,17 @@ class FileSystem {
 
   ls(path: string, wd: string): [boolean, string] {
     const filesPerLine = 4
-    const wd_node = get_node(wd, this.fileTree)
-    const parentNode = get_node(basename(path), wd_node)
 
-    if (parentNode === undefined) return [false, `ls: ${basename(path)}: No such file or directory`]
+    const fullPath = resolve(wd, path)
+    const node = get_node(fullPath, this.fileTree)
+
+    if (node === undefined) return [false, `ls: ${fullPath}: No such directory`]
 
     let paths = ''
-    parentNode.childrens.forEach((children, index) => {
-      paths += `${children.iNode.name}${children.iNode.nodeType === nodeType.folder ? '/' : ''}`
+    node.childrens.forEach((children, index) => {
+      paths += `${children.iNode.name}${
+        children.iNode.nodeType === nodeType.folder ? '/' : ''
+      }`
       paths += `${index % filesPerLine === filesPerLine - 1 ? '\n' : '\t'}`
     })
 
@@ -128,20 +128,20 @@ class FileSystem {
   }
 
   touch(path: string, wd: string): [boolean, string] {
-    const wd_node = get_node(wd, this.fileTree)
-    const parentNode = get_node(dirname(path), wd_node)
+    const fullPath = resolve(wd, path)
+    const parentNode = get_node(dirname(fullPath), this.fileTree)
 
-    if (parentNode === undefined) return [false, `touch: ${dirname(path)}: No such file or directory`]
+    if (parentNode === undefined)
+      return [false, `touch: ${dirname(fullPath)}: No such directory`]
 
     const block = findFreeBlock(this.hdd, this.block_size)
-    if (block === undefined) return [false, `touch: ${path}: No block free`]
+    if (block === undefined) return [false, `touch: ${fullPath}: No block free`]
     new DataView(this.hdd, block * this.block_size, 1).setUint8(0, 1)
-
 
     const name = basename(path)
     const new_file: fsNode = {
       iNode: create_node(name, nodeType.file, new Uint16Array([block])),
-      childrens: [undefined]
+      childrens: [undefined],
     }
 
     parentNode.childrens.push(new_file)
@@ -151,10 +151,11 @@ class FileSystem {
   }
 
   echo(path: string, wd: string, content: string): [boolean, string] {
-    const wd_node = get_node(wd, this.fileTree)
-    const node = get_node(basename(path), wd_node)
+    const fullPath = resolve(wd, path)
+    const node = get_node(fullPath, this.fileTree)
 
-    if (node === undefined) return [false, `echo: ${path}: No such file or directory`]
+    if (node === undefined)
+      return [false, `echo: ${fullPath}: No such file or directory`]
 
     const blocks_required = Math.ceil(content.length / this.block_size)
     const newChildrens = new Uint16Array(blocks_required)
@@ -164,7 +165,6 @@ class FileSystem {
       const block = findFreeBlock(this.hdd, this.block_size)
       if (block === undefined) return [false, `echo: ${path}: No block free`]
       new DataView(this.hdd, block * this.block_size, 1).setUint8(0, 1)
-
 
       newChildrens[node.iNode.childrensLength] = block
       node.iNode.childrensLength += 1
@@ -178,10 +178,11 @@ class FileSystem {
   }
 
   cat(path: string, wd: string): [boolean, string] {
-    const wd_node = get_node(wd, this.fileTree)
-    const node = get_node(basename(path), wd_node)
+    const fullPath = resolve(wd, path)
+    const node = get_node(fullPath, this.fileTree)
 
-    if (node === undefined) return [false, `cat: ${path}: No such file or directory`]
+    if (node === undefined)
+      return [false, `cat: ${fullPath}: No such file or directory`]
 
     return [true, read_file(this.hdd, node.iNode, this.block_size)]
   }
@@ -190,12 +191,103 @@ class FileSystem {
     const newWd = resolve(wd, path)
     const node = get_node(newWd, this.fileTree)
 
-    if (node === undefined) return [false, `cd: ${newWd}: No such file or directory`]
+    if (node === undefined) return [false, `cd: ${newWd}: No such directory`]
 
     return [true, newWd]
   }
 
+  rm(path: string, wd: string): [boolean, string] {
+    const fullPath = resolve(wd, path)
+    const node = get_node(fullPath, this.fileTree)
+    const parentNode = get_node(resolve(wd, path, '..'), this.fileTree)
+
+    if (
+      node === undefined ||
+      parentNode === undefined ||
+      node.iNode.nodeType === nodeType.folder
+    )
+      return [false, `rm: ${path}: No such file or directory`]
+
+    const index = parentNode.childrens.indexOf(node)
+    parentNode.childrens.splice(index, 1)
+
+    node.iNode.childrens.forEach((children) =>
+      freeBlock(this.hdd, children, this.block_size),
+    )
+
+    this.writeFS()
+    return [true, '']
+  }
+
+  rmdir(path: string, wd: string): [boolean, string] {
+    const node = get_node(resolve(wd, path), this.fileTree)
+    const parentNode = get_node(resolve(wd, path, '..'), this.fileTree)
+
+    if (
+      node === undefined ||
+      parentNode === undefined ||
+      node.iNode.nodeType === nodeType.file
+    )
+      return [false, `rmdir: ${path}: No such file or directory`]
+
+    if (node.childrens.length > 0)
+      return [false, `rmdir: ${path}: Directory not empty`]
+
+    const index = parentNode.childrens.indexOf(node)
+    parentNode.childrens.splice(index, 1)
+
+    this.writeFS()
+    return [true, '']
+  }
+
+  cp(from: string, to: string, wd: string): [boolean, string] {
+    const nodeFrom = get_node(resolve(wd, from), this.fileTree)
+
+    if (nodeFrom === undefined) return [false, `cp: ${from}: No such file`]
+
+    if (nodeFrom.iNode.nodeType === nodeType.folder)
+      return [false, `cp: ${from}: No copy directory`]
+
+    const [catStatusCode, catContent] = this.cat(from, wd)
+    if (!catStatusCode) return [catStatusCode, catContent]
+
+    const [touchStatusCode, touchOut] = this.touch(to, wd)
+    if (!touchStatusCode) return [touchStatusCode, touchOut]
+
+    const [echoStatusCode, echoOut] = this.echo(to, wd, catContent)
+    if (!echoStatusCode) return [echoStatusCode, echoOut]
+
+    this.writeFS()
+    return [true, '']
+  }
+
+  mv(from: string, to: string, wd: string): [boolean, string] {
+    const nodeFrom = get_node(resolve(wd, from), this.fileTree)
+
+    if (nodeFrom === undefined)
+      return [false, `mv: ${from}: No such file or directory`]
+
+    const fromDirname = dirname(resolve(wd, from))
+    const nodeFromDir = get_node(fromDirname, this.fileTree)
+    const indexFrom = nodeFromDir.childrens.indexOf(nodeFrom)
+
+    nodeFromDir.childrens.splice(indexFrom, 1)
+
+    const toDirname = dirname(resolve(wd, to))
+    const nodeToDir = get_node(toDirname, this.fileTree)
+
+    nodeToDir.childrens.push(nodeFrom)
+
+    nodeFrom.iNode.name = basename(to)
+    nodeFrom.iNode.nameLength = nodeFrom.iNode.name.length
+
+    this.writeFS()
+    return [true, '']
+  }
+
   private blocksInUse(blocks: number[], node: fsNode): number[] {
+    const nodeSize = get_inode_size(node.iNode) / this.block_size
+    blocks[0] += nodeSize
     if (node.iNode.nodeType === nodeType.file) {
       let used = node.iNode.contentSize / this.block_size
       let index = 0
@@ -209,42 +301,32 @@ class FileSystem {
       return blocks
     }
 
-    return node.childrens.reduce((prev, curr) => this.blocksInUse(prev, curr), blocks)
+    return node.childrens.reduce(
+      (prev, curr) => this.blocksInUse(prev, curr),
+      blocks,
+    )
   }
 
   diskUsed(): number[] {
-    return this.blocksInUse(new Array(this.blocks).fill(0), this.fileTree)
+    const disk = new Array(this.blocks).fill(0)
+    return this.blocksInUse(disk, this.fileTree)
   }
 
-  //dump(path: string) {
-  //  writeFileSync(path, Buffer.from(this.hdd))
-  //}
+  blocksUsed(): number {
+    let useds = 1
+    for (let i = 1; i < this.blocks; i++) {
+      const blockView = new DataView(this.hdd, i * this.block_size, 1)
+      if (blockView.getUint8(0) === 1) useds += 1
+    }
+
+    return useds
+  }
+
+  loadHddBuffer(hdd: ArrayBuffer) {
+    this.hdd = hdd
+    this.readFS()
+    this.writeFS()
+  }
 }
 
-//const fs = new FileSystem(block_size, blocks)
-
-//fs.mkdir('root', '/')
-//fs.mkdir('images', '/root')
-
-//fs.touch('boot.txt', '/')
-//fs.touch('init.bin', '/')
-
-//fs.echo('boot.txt', '/', ``)
-//fs.echo('init.bin', '/', 'wilgnne')
-
-
-
-//fs.ls('.', '/')
-//console.log(fs.cat('boot.txt', '/'));
-//console.log(fs.cat('init.bin', '/'));
-
-//console.log(fs.cd('../download', '/root/imagess'))
-
-//console.log(fs.diskUsed());
-
-
-//fs.show()
-//fs.dump('fs.bin')
-
-//export { fs }
 export default FileSystem
--- tests/FileSystem.spec.ts
+++ tests/FileSystem.spec.ts
@@ -8,6 +8,7 @@ describe('testing File System: File', () => {
 
   test('touch', () => {
     expect(fs.touch('boot.txt', '/')).toStrictEqual([true, ''])
+    expect(fs.touch('init.txt', '/')).toStrictEqual([true, ''])
   })
 
   test('echo', () => {
@@ -16,6 +17,21 @@ describe('testing File System: File', () => {
 
   test('cat', () => {
     expect(fs.cat('boot.txt', '/')).toStrictEqual([true, 'Teste Content'])
+  })
+
+  test('rm', () => {
+    expect(fs.rm('boot.txt', '/')).toStrictEqual([true, ''])
+    expect(fs.ls('.', '/')).toStrictEqual([true, 'init.txt\t'])
+  })
+
+  test('cp', () => {
+    expect(fs.cp('init.txt', 'step.txt', '/')).toStrictEqual([true, ''])
+    expect(fs.ls('.', '/')).toStrictEqual([true, 'init.txt\tstep.txt\t'])
+  })
+
+  test('mv', () => {
+    expect(fs.mv('init.txt', 'boot.txt', '/')).toStrictEqual([true, ''])
+    expect(fs.ls('.', '/')).toStrictEqual([true, 'step.txt\tboot.txt\t'])
   })
 })
 
@@ -27,15 +43,24 @@ describe('testing File System: Directory', () => {
 
   test('mkdir', () => {
     expect(fs.mkdir('root', '/')).toStrictEqual([true, ''])
+    expect(fs.mkdir('src', '/')).toStrictEqual([true, ''])
   });
 
   test('ls', () => {
-    expect(fs.ls('.', '/')).toStrictEqual([true, 'root/\t'])
+    expect(fs.ls('.', '/')).toStrictEqual([true, 'root/\tsrc/\t'])
   })
 
   test('cd', () => {
     expect(fs.cd('root', '/')).toStrictEqual([true, '/root'])
   })
+
+  test('rmdir', () => {
+    expect(fs.rmdir('src', '/')).toStrictEqual([true, ''])
+    expect(fs.ls('.', '/')).toStrictEqual([true, 'root/\t'])
+  })
+
+  test('mv', () => {
+    expect(fs.mv('root', 'bin', '/')).toStrictEqual([true, ''])
+    expect(fs.ls('.', '/')).toStrictEqual([true, 'bin/\t'])
+  })
 })
-
-
