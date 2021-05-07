069bd2563243cbfb49bc78e2e376cac571d14e98
Wilgnne Khawan <wilgnne.kba@gmail.com>
2021-05-07 15:48:37 +0000

    feat: FSHeatmap

--- src/components/FSHeatmap.tsx
+++ src/components/FSHeatmap.tsx
@@ -3,14 +3,15 @@ import dynamic from 'next/dynamic'
 const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
 import FileSystemContext from './FileSystemContext'
+import { Tabs, TabList, Tab, Wrap, WrapItem, Box } from '@chakra-ui/react'
 
-function genSeries(array: number[], rows: number, cols: number) {
+function genSeries(array: number[], rows: number, cols: number, initial = 0) {
   const series = []
   for (let i = rows - 1; i >= 0; i--) {
-    const name = `${i * cols}`
-    const data: { x: string, y: number }[] = []
+    const name = `${i * cols + initial}`
+    const data: { x: string; y: number }[] = []
     for (let j = 0; j < cols; j++) {
-      const offset = (i * cols) + j
+      const offset = i * cols + j
       const x = `${j}`
       const y = array[offset]
 
@@ -27,44 +28,82 @@ const options: ApexCharts.ApexOptions = {
     redrawOnParentResize: true,
     redrawOnWindowResize: true,
     animations: {
-      enabled: false
-    }
+      enabled: false,
+    },
   },
   dataLabels: {
-    enabled: false
+    enabled: false,
   },
-  colors: ["#008FFB"],
+  colors: ['#008FFB'],
   title: {
-    text: 'Disk HeatMap'
+    text: 'Disk HeatMap',
   },
 }
 
-const FSHeatmap = () => {
-  const [series, setSeries] = useState([])
+const rows = 32
+const columns = 32
+const ePerChart = rows * columns
+
+const FSHeatmap = ({ blocks }) => {
+  const [diskUsed, setdiskUsed] = useState<number[]>([])
+  const [indexes, setIndexes] = useState<any[]>([])
+  const [serie, setSerie] = useState<any[]>([])
+  const [tabIndex, setTabIndex] = useState(0)
 
   const { registerCallback } = useContext(FileSystemContext)
 
+  const handleTabsChange = (index) => {
+    setTabIndex(index)
+  }
+
   useEffect(() => {
-    console.log('registerCallback');
+    const initial = tabIndex * ePerChart
+    const serieBlocks = diskUsed.slice(initial, ePerChart)
+
+    setSerie(genSeries(serieBlocks, rows, columns, initial))
+  }, [diskUsed, tabIndex])
+
+  useEffect(() => {
+    console.log('registerCallback')
+
+    setIndexes(new Array(blocks / ePerChart).fill(0).map((_, index) => index))
 
     registerCallback((fs) => {
-      const diskUsed = fs?.diskUsed() || []
-
-      const newSeries = genSeries(diskUsed.map(value => value * 100), 32, 64)
-      setSeries(newSeries)
+      setdiskUsed(fs?.diskUsed() || [])
     })
   }, [])
 
   return (
-    <div id="chart">
-      <ReactApexChart
-        options={options}
-        series={series}
-        type="heatmap"
-        height="500px"
-      />
-    </div>
+    <Box borderRadius="md" borderWidth="2px" p="5px" h="100%">
+      <Tabs
+        isFitted
+        isLazy
+        size="sm"
+        index={tabIndex}
+        onChange={handleTabsChange}
+      >
+        <TabList>
+          <Wrap justify="center">
+            {indexes.map((value, index) => {
+              return (
+                <WrapItem>
+                  <Tab key={index}>{`${value}K`}</Tab>
+                </WrapItem>
+              )
+            })}
+          </Wrap>
+        </TabList>
+      </Tabs>
+      <Box minH="400px" h="auto">
+        <ReactApexChart
+          options={options}
+          series={serie}
+          type="heatmap"
+          height="100%"
+        />
+      </Box>
+    </Box>
   )
 }
 
-export default FSHeatmap;
+export default FSHeatmap
