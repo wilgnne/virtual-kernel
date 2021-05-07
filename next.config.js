dee6b21202ef6db0e58fed61501b07fab6c8f608
Wilgnne Khawan <wilgnne.kba@gmail.com>
2021-05-07 19:37:45 +0000

    chore: Use basePath

--- next.config.js
+++ next.config.js
@@ -4,7 +4,5 @@ const BACKEND_URL = isProd ? 'virtual-kernel/' : ''
 
 module.exports = {
   assetPrefix: BACKEND_URL,
-  env: {
-    BACKEND_URL: BACKEND_URL,
-  },
+  basePath: BACKEND_URL,
 }
--- src/components/Navbar/MenuItem.tsx
+++ src/components/Navbar/MenuItem.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 import { Text } from '@chakra-ui/react'
 
-
 const MenuItem = ({ children, to = '/', ...rest }) => {
   return (
-    <Link href={to} as={ process.env.BACKEND_URL + to}>
-      <Text display='block' {...rest}>
+    <Link href={to}>
+      <Text display="block" {...rest}>
         {children}
       </Text>
     </Link>
